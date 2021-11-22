f9972d718ec5c480406647bec3493a67774f4d18
dominick <dominickugas@gmail.com>
2021-11-21 22:19:53 -0500

    commit tree 73ec5f67a30191c6be58658d3b824bd42c355417

--- control_flow.rb
+++ control_flow.rb
@@ -1,13 +1,32 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" && password == "12345" )|| (username=="ADMIN" && password == "12345")
+     binding.pry
+    "Access granted"
+  else
+    # binding.pry
+    "Access denied"
+  end
+
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature =< 65
+    "It's a little chilly out there!"
+    elsif temperature > 85
+      "It's too dang hot out there!"
+    else
+      "It's perfect out there!"
 end
 
-def fizzbuzz(num)
-  # your code here
+def fizzbuzz(danny)
+  if danny%3 == 0 
+    "Fizz"
+    elsif danny%5 ==0
+    "Buzz"
+    else
+      danny
 end
 
 def calculator(operation, num1, num2)
